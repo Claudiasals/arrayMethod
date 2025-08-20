0b9f1c9267f9461688c5b59ceb701e099c60f034
Claudiasals <salsiniclaudia@gmail.com>
2025-08-20 15:50:02 +0200

    esercizio 2

--- esercizi/es.19-8.js
+++ esercizi/es.19-8.js
@@ -1,19 +1,20 @@
 // Esercizio: Dato un array di prodotti, filtra solo quelli disponibili in magazzino 
 // e restituisci un array con i loro nomi in maiuscolo 
-const products = [ 
-    { id: 1, name: "Laptop", price: 999, inStock: true }, 
-    { id: 2, name: "Mouse", price: 25, inStock: false }, 
-    { id: 3, name: "Keyboard", price: 75, inStock: true }, 
-    { id: 4, name: "Monitor", price: 350, inStock: false }, 
-    { id: 5, name: "Headphones", price: 85, inStock: true }, 
+/* const products = [
+    { id: 1, name: "Laptop", price: 999, inStock: true },
+    { id: 2, name: "Mouse", price: 25, inStock: false },
+    { id: 3, name: "Keyboard", price: 75, inStock: true },
+    { id: 4, name: "Monitor", price: 350, inStock: false },
+    { id: 5, name: "Headphones", price: 85, inStock: true },
 ];
 
 
 const productsInStock = products.filter((product) => {
-    return product.inStock; 
+    return product.inStock;
 });
 
 console.log(productsInStock);
+*/
 
 //const productsNotInStock
 
@@ -27,4 +28,27 @@ console.log(notavaibleDevice); //qui mi stampa nel console.log i prodotti non di
 // });
 
 // console.log(items);
+
+
+
+// Esercizio: Calcola il valore totale di tutti gli articoli nel carrello
+// considerando la quantità e applicando uno sconto del 10% se il totale supera 100
+
+const shoppingCart = [
+    { item: "Book", price: 15, quantity: 2 },
+    { item: "Pen", price: 2, quantity: 10 },
+    { item: "Notebook", price: 8, quantity: 3 },
+    { item: "Backpack", price: 45, quantity: 1 },
+];
+
+let totalCart = shoppingCart.reduce((acc, product) => {
+    return acc + product.price * product.quantity;
+}, 0
+);
+
+if (totalCart > 100) {
+        totalCart = totalCart - totalCart * 0.1;
+    }
+
+console.log("totale: €", totalCart);
 
